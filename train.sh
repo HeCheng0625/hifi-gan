65be7c6fd170a62a862b80d7dc3fd75a9c09f144
yuanchengwang <1057041679@qq.com>
2023-02-09 08:19:43 +0000

    commit

---
+++ train.sh
@@ -0,0 +1,12 @@
+python train.py \
+--input_wavs_dir="/blob/v-yuancwang/audio_editing_data/audioset96/wav" \
+--input_mels_dir="" \
+--input_training_file="/home/v-yuancwang/hifi-gan/audioset96_train.txt" \
+--input_validation_file="/home/v-yuancwang/hifi-gan/audioset96_val.txt" \
+--checkpoint_path="/blob/v-yuancwang/hifigan_cp" \
+--config="/home/v-yuancwang/hifi-gan/config_ours.json" \
+--training_epochs=100 \
+--stdout_interval=5 \
+--checkpoint_interval=5000 \
+--summary_interval=100 \
+--validation_interval=1000 \
