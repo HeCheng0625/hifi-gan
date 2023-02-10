f26decbcb0cfe3e063a9f7f702a036ebce73eb67
yuanchengwang <1057041679@qq.com>
2023-02-10 07:24:14 +0000

    commit

--- inference.sh
+++ inference.sh
@@ -1,4 +1,4 @@
-/opt/conda/envs/py3.7/bin/python /home/v-yuancwang/AudioEditing/hifi-gan/inference_e2e.py \
---input_mels_dir="/home/v-yuancwang/AudioEditing/hifigan_test_vae" \
---output_dir="/home/v-yuancwang/AudioEditing/hifigan_test_vae_out" \
+/opt/conda/envs/py3.7/bin/python /home/v-yuancwang/AudioEditing/hifi-gan/inference.py \
+--input_wavs_dir="/home/v-yuancwang/AudioEditing/hifigan_test_vae_gt/wav" \
+--output_dir="/home/v-yuancwang/AudioEditing/hifigan_test_vae_gt_out/wav" \
 --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_00260000"
