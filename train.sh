8575805db558548b35be7cddb245d452782d582e
yuanchengwang <yuanchengwang37@gmail.com>
2023-03-15 07:02:37 +0000

    commit

--- train.sh
+++ train.sh
@@ -1,13 +1,11 @@
-/opt/conda/envs/py3.7/bin/python /home/v-yuancwang/AudioEditing/hifi-gan/train.py \
---input_wavs_dir="/blob/v-yuancwang/audio_editing_data/audiocaps/wav" \
---input_mels_dir="/blob/v-yuancwang/audio_editing_data/audiocaps/vae_out" \
---input_training_file="/home/v-yuancwang/AudioEditing/hifi-gan/audiocaps_train.txt" \
---input_validation_file="/home/v-yuancwang/AudioEditing/hifi-gan/audiocaps_val.txt" \
+python /home/v-yuancwang/hifi-gan/train.py \
+--input_wavs_dir="/blob/v-yuancwang/audio_editing_data/audioset96/wav" \
+--input_training_file="/home/v-yuancwang/hifi-gan/audioset96_train.txt" \
+--input_validation_file="/home/v-yuancwang/hifi-gan/audioset96_val.txt" \
 --checkpoint_path="/blob/v-yuancwang/hifigan_cp" \
---config="/home/v-yuancwang/AudioEditing/hifi-gan/config_ours.json" \
---training_epochs=5 \
+--config="/home/v-yuancwang/hifi-gan/config_ours.json" \
+--training_epochs=500 \
 --stdout_interval=5 \
 --checkpoint_interval=5000 \
 --summary_interval=100 \
---validation_interval=1000 \
---fine_tuning=True
+--validation_interval=1000
